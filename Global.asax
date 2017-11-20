d1bd5f12c4f9f618570b1e64ee8e6e4bec693db1
marcosalves1973 <33723235+marcosalves1973@users.noreply.github.com>
2017-11-20 08:21:37 -0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LojaSistema2.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LojaSistema2.Startup))]
+namespace LojaSistema2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
